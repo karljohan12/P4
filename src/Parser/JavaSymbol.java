32489457a5944ae50e91a6f6c48deec1659a0a0b
Emil Laurits Bech <ebech18@student.aau.dk>
2020-04-14 23:30:58 +0200

    Linenumber fix

--- src/Parser/JavaSymbol.java
+++ src/Parser/JavaSymbol.java
@@ -4,33 +4,30 @@ public class JavaSymbol extends java_cup.runtime.Symbol {
   private int line;
   private int column;
 
-  public JavaSymbol(int type, int line, int column) {
-    this(type, line, column, -1, -1, null);
+  public JavaSymbol(int id, int l, int r) {
+    super(id, l, r, (Object)null);
   }
 
-  public JavaSymbol(int type, int line, int column, Object value) {
-    this(type, line, column, -1, -1, value);
-  }
-
-  public JavaSymbol(int type, int line, int column, int left, int right, Object value) {
-    super(type, left, right, value);
-    this.line = line;
-    this.column = column;
+  public JavaSymbol(int id, int l, int r, Object o) {
+    super(id);
+    super.left = l;
+    super.right = r;
+    super.value = o;
   }
 
   public int getLine() {
-    return line;
+    return left;
   }
 
   public int getColumn() {
-    return column;
+    return right;
   }
 
   public String toString() {
     return "line "
-        + line
+        + left
         + ", column "
-        + column
+        + right
         + ", sym: "
         + sym
         + (value == null ? "" : (", value: '" + value + "'"));
