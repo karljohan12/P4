7c7d7331b3241e2cfef732e5ae4766799bef7da2
Emil Laurits Bech <ebech18@student.aau.dk>
2020-05-14 00:14:43 +0200

    Arduino track works

--- src/JavaParser.java
+++ src/JavaParser.java
@@ -26,6 +26,7 @@ import Parser.*;
 import SymbolTable.SymbolTable;
 import java_cup.runtime.Symbol;
 
+
 /**
  * Simple test driver for the java parser. Just runs it on some input files, gives no useful output.
  */
@@ -54,9 +55,10 @@ public class JavaParser {
         System.out.println("Writing to file...");
         fileWriter.write(cgv.code.toString());
         fileWriter.close();
-        System.out.println("Verifying storage...");
-
-        verify();
+        if(System.getProperty("os.name").startsWith("Windows")) {
+          System.out.println("Verifying storage...");
+          verify();
+        }
 
         System.out.println("Compilation succeeded");
       } catch (Exception e) {
@@ -65,43 +67,58 @@ public class JavaParser {
       }
     }
   }
-  public static void verify() throws InterruptedException, IOException {
-    ProcessBuilder process = new ProcessBuilder("C:\\Program Files (x86)\\Arduino\\arduino_debug", "--verify", "program.ino");
-    process.redirectOutput(new File("OutputExe.txt"));
-    process.redirectError(new File("ErrorOutputExe.txt"));
-    process.redirectInput(new File("InputExe.txt"));
-    Process e = process.start();
-    e.waitFor();
-    String storage = Files.readString(Paths.get("OutputExe.txt"));
-    System.out.println(storage);
+  public static void verify() throws RuntimeException, IOException, InterruptedException {
+    try {
 
-    String pattern1 = "(";
-    String pattern2 = "%)";
-    String text = storage;
-    ArrayList<String> percent = new ArrayList<String>();
+      String value = WindowsReqistry.readRegistry("HKLM\\SOFTWARE\\Classes\\Arduino file\\shell\\open\\command", "");
 
-    Pattern p = Pattern.compile(Pattern.quote(pattern1) + "(.*?)" + Pattern.quote(pattern2));
-    Matcher m = p.matcher(text);
-    while (m.find()) {
-      percent.add(m.group(1));
+
+      String[] pathArray = value.split("\"");
+      int indexOfExe = pathArray[1].indexOf("arduino.exe");
+      String path = pathArray[1].substring(0, indexOfExe);
+      path = path + "arduino_debug";
+
+      //ProcessBuilder process = new ProcessBuilder("C:\\Program Files (x86)\\Arduino\\arduino_debug", "--verify", "program.ino");
+      ProcessBuilder process = new ProcessBuilder(path, "--verify", "program.ino");
+      process.redirectOutput(new File("OutputExe.txt"));
+      process.redirectError(new File("ErrorOutputExe.txt"));
+      process.redirectInput(new File("InputExe.txt"));
+      Process e = process.start();
+      e.waitFor();
+      String storage = Files.readString(Paths.get("OutputExe.txt"));
+      System.out.println(storage);
+
+      String pattern1 = "(";
+      String pattern2 = "%)";
+      String text = storage;
+      ArrayList<String> percent = new ArrayList<String>();
+
+      Pattern p = Pattern.compile(Pattern.quote(pattern1) + "(.*?)" + Pattern.quote(pattern2));
+      Matcher m = p.matcher(text);
+      while (m.find()) {
+        percent.add(m.group(1));
+      }
+
+      if (percent.size() == 2) {
+
+        if (Integer.parseInt(percent.get(0)) > 100) {
+          System.out.println("Error: Program storage exceeds the maximum limit");
+          PrintWriter pw = new PrintWriter("program.ino");
+          pw.close();
+          throw new RuntimeException();
+        }
+        if (Integer.parseInt(percent.get(1)) > 100) {
+          System.out.println("Error: Dynamic storage exceeds the maximum limit");
+          PrintWriter pw = new PrintWriter("program.ino");
+          pw.close();
+          throw new RuntimeException();
+        } else if (Integer.parseInt(percent.get(1)) >= 80) {
+          System.out.println("Warning: Dynamic storage exceeds 80 %, which can cause stability issues");
+        }
+      }
     }
-
-    if(percent.size() == 2){
-      if(Integer.parseInt(percent.get(0)) > 100){
-        System.out.println("Error: Program storage exceeds the maximum limit");
-        PrintWriter pw = new PrintWriter("program.ino");
-        pw.close();
-        throw new RuntimeException();
-      }
-      if(Integer.parseInt(percent.get(1)) > 100){
-        System.out.println("Error: Dynamic storage exceeds the maximum limit");
-        PrintWriter pw = new PrintWriter("program.ino");
-        pw.close();
-        throw new RuntimeException();
-      }
-      else if(Integer.parseInt(percent.get(1)) >= 80){
-        System.out.println("Warning: Dynamic storage exceeds 80 %, which can cause stability issues");
-      }
+    catch (NullPointerException e){
+      System.out.println("Arduino installation could not be found, unable to determine size");
     }
 
   }
