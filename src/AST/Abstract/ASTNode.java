002bf495dacda2825243b08b8b26d9ae5b613e68
dnbech <dnbech@users.noreply.github.com>
2020-05-27 13:57:42 +0200

    Added INode

--- src/AST/Abstract/ASTNode.java
+++ src/AST/Abstract/ASTNode.java
@@ -1,6 +1,8 @@
 package AST.Abstract;
 
-abstract public class ASTNode {
+import Interfaces.INode;
+
+abstract public class ASTNode implements INode {
     public int lineNumber;
 
     public ASTNode(int ln) {lineNumber = ln;}
---
+++ src/Interfaces/INode.java
@@ -0,0 +1,5 @@
+package Interfaces;
+
+public interface INode {
+    public void accept(IVisitor v);
+}
